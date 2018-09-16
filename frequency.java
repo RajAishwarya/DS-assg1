f6e63553050b87089a1e7145ab04a80265183de2
Aishwarya Raj <42268889+RajAishwarya@users.noreply.github.com>
2018-09-16 09:06:43 +0530

    Update frequency.java

--- frequency.java
+++ frequency.java
@@ -8,34 +8,44 @@ public class frequency{
   public static char[] highestFreq(String str)
   {
     char c[]=str.toCharArray();
-    int max=-1,k=0;
+    int max=-1,k=0,m = 0;
     int c1[]= new int[c.length];
     char a[] = new char[c.length];
          for(int i=0;i<c.length;i++){
           for(int j=i+1;j<c.length;j++){
-            if(c[i]==c[j])
-                c1[i]+=1;
-               }
+            if(c[i]==c[j] && c[i] != '/')
+            {
+              c[j] = '/';
+              c1[i]+=1;
+              }
+          }
              if(max<c1[i])  max=c1[i];
               }
         for(int i = 0; i < c.length; i++)
         {
-
+          for(int j = i + 1; j < c.length; j++)
+          {
+            if(c[i] == c[j]) c1[i] = -1;
+          }
+        }
+        for(int i = 0; i < c.length; i++)
+        {
+          // System.out.print(max+" ");
           if(c1[i] == max)
           {
-            ch++;
-           a[i] = c[i];
+            // System.out.print(c[i]+" ");
+           a[m++] = c[i];
           }
         }
     return a;
   }
-	public static void main(String[] ar){
-		Scanner s=new Scanner(System.in);
-		String str=s.nextLine();
+  public static void main(String[] ar){
+    Scanner s=new Scanner(System.in);
+    String str=s.nextLine();
     char []high = highestFreq(str);
-    int k = getC();
-
-    for(int i = 0; i < k; i++)
-    System.out.println("Highest frequency is of "+high[i]);
-	}
+    //int k = getC();
+   // System.out.print(k+" ");
+    for(int i = 0; i < high.length; i++)
+    System.out.print(high[i]+" ");
+  }
 }
